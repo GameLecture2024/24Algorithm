4c3b0bfe420aeb7ad8a12f94934f569ba6ee8b94
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2024-10-23 17:45:53 +0900

    (2024.10.23) 동적계획법_메모이제이션 문제

--- Algorithm/DynamicProgramming.cpp
+++ Algorithm/DynamicProgramming.cpp
@@ -1,4 +1,6 @@
 #include "DynamicProgramming.h"
+#define DEFAULT -1
+std::vector<int> stairDP;
 
 int Fibonacci(int n)
 {
@@ -27,4 +29,49 @@ int Fibonacci_DP(int n)
     }
 
     return dp[n];
+}
+
+// 계단 0층 , {10, 20, 15 , 25, 10 ,20 }
+int max_stair_recursive(int i, std::vector<int> stairs)
+{
+    // stairDP 데이터가 -1이 아니면 (데이터 메모가 되어 있으면)
+    // 해당 데이터를 리턴
+    if (stairDP[i] != -1)
+    {
+        return stairDP[i];
+    }
+
+    // 계단을 오르는 아이디어
+
+    // 계단 1층 오를때 가장 큰 데이터
+    if (i == 0)
+    {
+        return  stairs[0];
+    }
+
+    // 계단 2층일 때 최대 값
+    if (i == 1)
+    {
+        return stairs[0] + stairs[1];
+    }
+
+    // 계단 3층일 때 최대 값
+    if (i == 2)
+    {
+        return std::max(stairs[0] + stairs[2], stairs[1]); // 
+    }
+
+    stairDP[i] = std::max(max_stair_recursive(i - 2, stairs),
+        max_stair_recursive(i - 3, stairs) + stairs[i - 1]) + stairs[i];
+
+    return stairDP[i];
+}
+
+int StairCount(std::vector<int> stairs)
+{
+    int size = stairs.size();
+
+    stairDP.assign(size, DEFAULT); // Default값이 -1이다.
+
+    return max_stair_recursive(size - 1, stairs);  
 }
--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -50,7 +50,7 @@ int main()
 
 	std::cout << "재귀 방식 : 피보나치 수열" << std::endl;
 
-	int count = 40;
+	int count = 10;
 
 	std::cout << "피보나치 수 결과:" << Fibonacci(count) << std::endl;
 	std::cout << "피보나치 수 시간:" << measureExecutionTime(Fibonacci, count) << "ms" << std::endl;
@@ -60,4 +60,10 @@ int main()
 	std::cout << "피보나치 수 결과:" << Fibonacci_DP(count) << std::endl;
 	std::cout << "피보나치 수 시간:" << measureExecutionTime(Fibonacci_DP, count) << "ms" << std::endl;
 
+
+	std::cout << "계단 오르기 문제 (메모이제이션)" << std::endl;
+
+	std::vector<int> stairs = { 10, 20, 15 , 25, 10 ,20 , 80 };
+
+	std::cout << "계단의 수가 (" << stairs.size() << ")인 최대 값 : " << StairCount(stairs) << std::endl;
 }
